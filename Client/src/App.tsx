5d0a0cceb36a21ec2d5dcf78a7490fcae0f12cf9
Anubhav <anubhav.joria@antino.com>
2025-03-19 12:13:26 +0530

    changed waitList page route

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -34,7 +34,7 @@ const App: React.FC = () => {
           />
           <Route path="/editscene" element={<EditScenes />} />
           <Route path="/finalvideo" element={<FinalVideoPage />} />
-          <Route path="/waitingList" element={<WaitingList />} />
+          <Route path="/waitList" element={<WaitingList />} />
         </Routes>
       </PersistGate>
     </Provider>
