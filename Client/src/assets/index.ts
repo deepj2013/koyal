c7839193130e125641fee265fc70f7bdb46254ff
Anubhav <anubhav.joria@antino.com>
2025-04-02 15:15:59 +0530

    assets added

---
+++ Client/src/assets/index.ts
@@ -0,0 +1,17 @@
+import RemoveSvg from './images/RemoveSvg.svg'
+import ClockSvg from './images/Time.svg'
+import ArrowUpSvg from './images/ArrowUpSvg.svg'
+import Realstic from "./images/realistic_preview.png";
+import Animated from "./images/animated_preview.png";
+import Sketch from "./images/sketch_preview.png";
+import Pencil from "./images/Pencil.svg";
+import Generate from "./images/Generate.svg";
+
+export const removeSvg = RemoveSvg;
+export const clockSvg = ClockSvg;
+export const arrowUpSvg = ArrowUpSvg;
+export const realisticStyle = Realstic;
+export const animatedStyle = Animated;
+export const sketchStyle = Sketch;
+export const pencilIcon = Pencil;
+export const GenerateIcon = Generate;
