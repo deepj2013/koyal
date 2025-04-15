f740305553b9531ea08e7f9873e980f896c23037
Anubhav <anubhav.joria@antino.com>
2025-04-15 08:56:43 +0530

    cors error resolve

--- Client/src/config/config.ts
+++ Client/src/config/config.ts
@@ -3,6 +3,6 @@ export const config = {
   regionName: import.meta.env.VITE_AWS_REGION,
   awsAccessKey: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
   awsSecretKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
-  baseUrl: import.meta.env.VITE_S3_BASE_URL,
+  baseUrl: "https://koyal.ai/v1",
   waitingListPassword: import.meta.env.VITE_WAITING_LIST_PASSWORD,
 };
