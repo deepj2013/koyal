6d3736b2a7c9c683e47fdacc517d58cc1ccdb1a4
Anubhav <anubhav.joria@antino.com>
2025-04-15 08:24:35 +0530

    .

--- Client/src/config/config.ts
+++ Client/src/config/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   bucketName: import.meta.env.VITE_S3_BUCKET_NAME,
-  regionName: import.meta.env.VITE_AWS_REGION,
+  regionName: 'ap-south-1',
   awsAccessKey: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
   awsSecretKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
   baseUrl: 'https://koyal.ai/v1',
